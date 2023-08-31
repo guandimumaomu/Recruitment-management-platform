ebf9cb263af54902d9383e8f7bb42ac124689a23
guandimumaomu <guandimumaomu@126.com>
2023-08-31 11:47:35 +0800

    搭建主面板骨架结构

--- recruitment-admin/src/App.vue
+++ recruitment-admin/src/App.vue
@@ -1,23 +1,11 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import MainLayout from './layout/index.vue'
 </script>
 
 <template>
-  <HelloWorld msg="Vite + Vue" />
-  <a-button type="primary">按钮</a-button>
+  <MainLayout/>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>

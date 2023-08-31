8ca1b9ea592c6982fef9674420020f35db61cdc4
guandimumaomu <guandimumaomu@126.com>
2023-08-31 11:27:26 +0800

    Ant Design of Vue组件库配置

--- recruitment-admin/vite.config.ts
+++ recruitment-admin/vite.config.ts
@@ -1,10 +1,20 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import {join} from "path"
+import { join } from "path"
+import Components from "unplugin-vue-components/vite";
+import { AntDesignVueResolver } from "unplugin-vue-components/resolvers";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    Components({
+      resolvers: [AntDesignVueResolver({
+        importStyle: false, // css in js
+      })
+      ],
+    }),
+  ],
 
   resolve: {
     alias: {

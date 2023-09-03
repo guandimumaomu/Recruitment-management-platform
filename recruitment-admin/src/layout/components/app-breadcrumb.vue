d49878f75986478a59947f718e05b7d3f8dce7e3
guandimumaomu <guandimumaomu@126.com>
2023-09-03 20:00:17 +0800

    面包屑实现注释

--- recruitment-admin/src/layout/components/app-breadcrumb.vue
+++ recruitment-admin/src/layout/components/app-breadcrumb.vue
@@ -16,13 +16,19 @@ import { ref } from 'vue';
 const route = useRoute()
 const routeMap = routeMapTool()
 // console.log(routeMap)
-const pathList = ref<string[]>()
+const pathList = ref<string[]>([]) //默认空数组
 
 const pathToArr = (path: string) => {
+  console.log(path)
+  //"/category/pub"
+  
   let arr = path.split('/').filter(i => i)
   let pathArr = arr.map((_, index) => {
     return '/'+arr.slice(0, index+1).join('/')
   })
+  console.log(pathArr)
+  //  ['/category', '/category/pub']
+  
   return pathArr
 }
 
@@ -30,9 +36,12 @@ const pathToArr = (path: string) => {
 
 watch(route, (newVal) => {
   // console.log("路由变化了", newVal.path)
-  pathList.value = pathToArr(newVal.path)
+  //在 Vue 3 中，响应式变量的值可以通过 .value 属性进行读取和修改。
 
-  
+  //路径发生了变化，立即调用pathArr函数，将路径变为路径数组
+  //遍历路径数组，生成对应的文本标签
+  pathList.value = pathToArr(newVal.path)
+  console.log(routeMap)
 })
 
 </script>

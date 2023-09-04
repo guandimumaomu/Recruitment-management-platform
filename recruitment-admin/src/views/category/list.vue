1882cb98e2ad886ceb34fcbbf2b216df1b3c5aaf
guandimumaomu <guandimumaomu@126.com>
2023-09-04 13:34:10 +0800

    a-table实现分类列表渲染

--- recruitment-admin/src/views/category/list.vue
+++ recruitment-admin/src/views/category/list.vue
@@ -1,13 +1,43 @@
-<script setup lang="ts">
-</script>
-
-
 <template>
-    <div>
-        岗位列表
-    </div>
-</template>
+    <a-table :columns="columns" :data-source="data">
+      
+      <!-- 插槽 -->
+      <template #bodyCell="{ column}">  
 
-
-<style scoped>
-</style>
+        <template v-if="column.key === 'action'">
+            <a-space>
+                <a-button type="primary" size="small">编辑</a-button>
+                <a-button type="primary" danger size="small">删除</a-button>
+            </a-space> 
+        </template>
+      </template>
+    </a-table>
+  </template>
+  <script lang="ts" setup>
+  import { categoryGet } from '@/api/pro';
+import { CategoryType } from '@/types/pro';
+import { ref } from 'vue';
+  const columns = [
+    {
+      title: '分类名称',
+      dataIndex: 'name',
+      key: 'name',
+    },
+    {
+      title: '分类图标',
+      dataIndex: 'icon',
+      key: 'icon',
+    },
+    {
+      title: '操作',
+      key: 'action',
+    },
+  ];
+  
+  const data = ref<Array<CategoryType>>([])
+  categoryGet(true).then(res => {
+    data.value = res.data.results
+  })
+  </script>
+  
+  
